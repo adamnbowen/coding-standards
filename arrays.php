6df8af8de4594422d4431f559f667fafdf6a0776
Bauffman <davy@spoon-library.com>
2011-08-05 15:48:25 +0200

    Created arrays example file. More content needs to come.

---
+++ arrays.php
@@ -0,0 +1,28 @@
+<?php
+
+// @todo add some more examples
+
+// simple array
+$array = array(
+	1, 2, 3,
+	'one', 'two', 'three',
+	$one, $two, $three
+);
+
+// basic associative array
+	$user = array(
+		'name' => 'Davy Hellemans',
+		'email' => 'davy@spoon-library.com'
+	);
+
+// multi dimensional arrays
+$users = array(
+	1 => array(
+		'name' => 'Tijs Verkoyen',
+		'email' => 'tijs@sumocoders.be'
+	),
+	2 => array(
+		'name' => 'Matthias Mullie',
+		'email' => 'matthias.mullie@netlash.com'
+	)
+);
