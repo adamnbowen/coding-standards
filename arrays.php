e9535d99c611bda5d0e1780188884e7248e7e081
Bauffman <davy@spoon-library.com>
2011-08-24 11:13:05 +0200

    Removed trailing comma for now. Is up for discussion.

--- arrays.php
+++ arrays.php
@@ -6,23 +6,23 @@
 $array = array(
 	1, 2, 3,
 	'one', 'two', 'three',
-	$one, $two, $three,
+	$one, $two, $three
 );
 
 // basic associative array
 $user = array(
 	'name' => 'Davy Hellemans',
-	'email' => 'davy@spoon-library.com',
+	'email' => 'davy@spoon-library.com'
 );
 
 // multi dimensional arrays
 $users = array(
 	1 => array(
 		'name' => 'Tijs Verkoyen',
-		'email' => 'tijs@sumocoders.be',
+		'email' => 'tijs@sumocoders.be'
 	),
 	2 => array(
 		'name' => 'Matthias Mullie',
-		'email' => 'matthias.mullie@netlash.com',
-	),
+		'email' => 'matthias.mullie@netlash.com'
+	)
 );
